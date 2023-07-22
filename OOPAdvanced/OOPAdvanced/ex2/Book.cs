07f3fa547ff80adec9e3d62dc050d71ed50f2bd6
Rebeca Bagu <rebecarisciuc@yahoo.com>
2023-07-22 17:56:45 +0300

    add ex 2

---
+++ OOPAdvanced/OOPAdvanced/ex2/Book.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace OOPAdvanced.ex2
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private int pageCount;
+        private int currentPage;
+
+        public Book(string title, string author, int pageCount)
+        {
+            this.title = title;
+            this.author = author;
+            this.pageCount = pageCount;
+            this.currentPage = 1;
+        }
+
+        public string GetTitle()
+        {
+            return title;
+        }
+
+        public string GetAuthor()
+        {
+            return author;
+        }
+
+        public int GetPageCount()
+        {
+            return pageCount;
+        }
+
+        public int GetCurrentPage()
+        {
+            return currentPage;
+        }
+
+        public void NextPage()
+        {
+            if (currentPage < pageCount)
+                currentPage++;
+        }
+
+        public void PreviousPage()
+        {
+            if (currentPage >= 1)
+                currentPage--;
+        }
+
+        public string Title { get { return title; } set { title = value; } }
+        public string Author { get { return author; } set { author = value; } }
+    }
+}
