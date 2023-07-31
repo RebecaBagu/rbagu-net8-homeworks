ee3db4d3ce21375a2e994b23a71819e5a722fc92
RebecaBagu <rebecarisciuc@yahoo.com>
2023-07-31 13:05:56 +0300

    .

---
+++ ExceptionsLINQlambdas/ExceptionsLINQlambdas/Exercise10&11.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Product
+{
+	public Product() { }
+	public string Name { get; set; }
+	public string Category { get; set; }
+	public int Price { get; set; }
+}
+
+
+public class Exercise10
+{
+	public static void  Ex10(List<Product>products)
+	{
+		var result = products.GroupBy(x => x.Category)
+			.Select(x => new { category = x.Key, list = x.OrderBy(x => x.Price).ThenBy(x=>x.Name) });
+
+		foreach(var cat in result)
+		{
+			foreach(var item in cat.list)
+			{
+				Console.WriteLine($"{item.Category}, Price: {item.Price}, Name: {item.Name}");
+			}
+		}
+	}
+	public static void Ex11(List<Product> products)
+	{
+		var result = products.GroupBy(x => x.Category)
+			.Select(x => new { category = x.Key, avg = x.Average(p => p.Price) })
+			.OrderBy(x=>x.avg)
+			.Select(x=>x.category).ToList();
+
+		foreach(var cat in result)
+		{
+			Console.WriteLine(cat);
+		}
+
+
+    }
+}
+
+
--- ExceptionsLINQlambdas/ExceptionsLINQlambdas/Program.cs
+++ ExceptionsLINQlambdas/ExceptionsLINQlambdas/Program.cs
@@ -1,20 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//Exercise1.Ex1();
+Exercise1.Ex1();
+Console.WriteLine();
 
-//Exercise2.Ex2();
+Exercise2.Ex2();
+Console.WriteLine();
 
-//Exercise3.Ex3();
+Exercise3.Ex3();
+Console.WriteLine();
 
-//Exercise4.Ex4();
+Exercise4.Ex4();
+Console.WriteLine();
 
-//DateTime today = DateTime.Now;
-//Console.WriteLine(  Exercise5.ToFullDateString(today));
+DateTime today = DateTime.Now;
+Console.WriteLine(  Exercise5.ToFullDateString(today));
+Console.WriteLine();
 
-//Console.WriteLine(Exercise6.ToBinary(25) );
+Console.WriteLine(Exercise6.ToBinary(25) );
+Console.WriteLine();
 
-//Exercise7.Ex7();
+Exercise7.Ex7();
+Console.WriteLine();
 
-//Exercise8.Ex8();
+Exercise8.Ex8();
+Console.WriteLine();
 
 
 //-------Ex9
@@ -26,9 +34,10 @@ var students = new List<Student> {
   new Student { Name = "Dave", Grade = "C" }
 };
 
-//Exercise9.Ex9(students);
+Exercise9.Ex9(students);
 
 
+Console.WriteLine();
 
 
 //---------------Ex10
@@ -40,4 +49,8 @@ var products = new List<Product> {
   new Product { Name = "Product 4", Category = "Category 3", Price = 5 },
   new Product { Name = "Product 5", Category = "Category 3", Price = 5 }
 };
+
+Exercise10.Ex10(products);
+Console.WriteLine();
+Exercise10.Ex11(products);
 
